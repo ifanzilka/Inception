cf5b30af8602bf809df011b50aac343e82baa649
F@nzil <nabiyarov.fanzil@gmail.com>
2021-10-14 00:48:45 +0300

    Update mariadb_start.sh

--- Inception/srcs/requirements/mariadb/tools/mariadb_start.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -13,20 +13,30 @@ service mysql start
 find_my_user=$(echo "SELECT USER from mysql.user;" | mysql --no-defaults -u root | grep "$DB_USER" | wc -l)
 
 # -ne (not equal)
-if [ "1" -ne $find_my_user ] ;
-	then 
-		echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql --no-defaults -u root
-		echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION;" | mysql --no-defaults -u root
-		echo "FLUSH PRIVILEGES;" | mysql --no-defaults -u root ;
+if [ ! -d /var/lib/mysql/wordpress ]; then
+
+	service mysql start
+	# Проверяем существует ли пользователь, если нет то создаем
+	find_my_user=$(echo "SELECT USER from mysql.user;" | mysql --no-defaults -u root | grep "$DB_USER" | wc -l)
+
+	# -ne (not equal)
+	if [ "1" -ne $find_my_user ] ;
+		then 
+			echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql --no-defaults -u root
+			echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION;" | mysql --no-defaults -u root
+			echo "FLUSH PRIVILEGES;" | mysql --no-defaults -u root ;
+	fi
+
+	# Проверяем существует ли база данных, если нет то создаем
+	find_my_database=$(echo "SHOW DATABASES;" | mysql --no-defaults -u root | grep "$DB_NAME" | wc -l)
+
+	if [ "1" -ne $find_my_database ] ;
+		then echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql --no-defaults -u $DB_USER --password="$DB_PASSWORD" ;
+		echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');" | mysql --no-defaults -u root ;
+	fi
+	service mysql stop
 fi
 
-# Проверяем существует ли база данных, если нет то создаем
-find_my_database=$(echo "SHOW DATABASES;" | mysql --no-defaults -u root | grep "$DB_NAME" | wc -l)
-
-if [ "1" -ne $find_my_database ] ;
-	then echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql --no-defaults -u $DB_USER --password="$DB_PASSWORD" ;
-	echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');" | mysql --no-defaults -u root ;
-fi
 
 service mysql stop
 
